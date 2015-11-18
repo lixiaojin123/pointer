bd8caf86248ffd295d1475cec97be1bcfbea6fad
lixiaojin123 <20141105062l@FanydeiMac13.local>
2015-11-18 20:32:32 +0800

    第五次提交
    在第四次提交的基础上再输出，这次是通过指针p来访问，指针p中存放的是a的地址，通过间接取值得出a[2] 的值

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@
         a[3]=40;
         p=a;
         printf("&a[0]=%x &a[1]=%x &a[2]=%x &a[3]=%x a=%x \n",&a[0],&a[1],&a[2],&a[3],a);
-        printf("a[0]=%d \n",*a);
+        printf("a[0]=%d \n",*(p+2));
         return 0;
     }
 
