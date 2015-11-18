e76e96eed066d2a93edf75df344555277400af76
lixiaojin123 <20141105062l@FanydeiMac13.local>
2015-11-18 19:53:03 +0800

    第二次提交
    定义一个变量a，给它赋初始值10，定义一个指针p，指针p中存放的是a变量的地址，输出a变量的地址（读入十六进制整数），a变量的值（读入十六进制整
    数），p变量的地址（读入十六进制整数），p中存放的a变量的地址（读入十六进制整数），通过a变量的地址取到a变量的值（间接取值）（读入十六进制整数
    
    ）输出a变量的地址（读入十进制整数），a变量的值（读入十进制整数），p变量的地址（读入十进制整数），p中存放的a变量的地址（读入十进制整数），通
    过a变量的地址取到a变量的值（间接
    取值）（读入十进制整数 ）

--- main.cpp
+++ main.cpp
@@ -5,11 +5,15 @@
 //  Created by 李晓晋 on 15/11/18.
 //  Copyright © 2015年 李晓晋. All rights reserved.
 //
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a=10;
+    int *p;
+    p=&a;
+    printf("&a=%x a=%x &p=%x p=%x *p=%x \n",&a,a,&p,p,*p);
+    printf("&a=%d a=%d &p=%d p=%d *p=%d \n",&a,a,&p,p,*p);
     return 0;
 }
+
