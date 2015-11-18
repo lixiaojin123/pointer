118532ef019d47ebfe16d924294e427003dfe51a
lixiaojin123 <20141105062l@FanydeiMac13.local>
2015-11-18 19:59:47 +0800

    第三次提交
    定义一个数组a，数组长度为4，第一个数组元素a[0]赋值为为10，第二个数组元素a[1]赋值为为20，第三个数组元素a[2]赋值为为30，第四个
    数组元素a[3]赋值为为40，输出数组中元素a[0],a[1],a[2],a[3],a 的地址也就是p的地址

--- main.cpp
+++ main.cpp
@@ -7,13 +7,18 @@
 //
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    int a=10;
-    int *p;
-    p=&a;
-    printf("&a=%x a=%x &p=%x p=%x *p=%x \n",&a,a,&p,p,*p);
-    printf("&a=%d a=%d &p=%d p=%d *p=%d \n",&a,a,&p,p,*p);
-    return 0;
-}
+
+    int main(int argc, const char * argv[]) {
+        // insert code here...
+        int a[4];
+        int *p;
+        a[0]=10;
+        a[1]=20;
+        a[2]=30;
+        a[3]=40;
+        p=a;
+        printf("&a[0]=%x &a[1]=%x &a[2]=%x &a[3]=%x a=%x \n",&a[0],&a[1],&a[2],&a[3],a);
+        return 0;
+    }
+
 
