ce9386c16b0b6de652cdf9237c5c449a921fd6e0
lixiaojin123 <20141105062l@FanydeiMac13.local>
2015-12-09 19:21:36 +0800

    第十四次提交
    定义一个学生类，学生类中存放的是学生的姓名，性别，年龄，再定义一个尾部指针next，尾部指针next中存放的是下个数组的地址（链表）分别定义一个
    类的四个对象A,B,C,D分别输入类的四个对象a,b,c,d中的姓名和年龄，然后输出

--- main.cpp
+++ main.cpp
@@ -9,13 +9,20 @@
 struct student
 {
     char name[100];
-    char sex;
     int age;
     student *next;
 };
 int main(int argc,const char *argv[]){
     //insert code here...
     student A,B,C,D;
-    printf("%x ,%x, %x, %x",&A,&B,&C,&D);
+    scanf("%s%d",A.name,&A.age);
+    scanf("%s%d",B.name,&B.age);
+    scanf("%s%d",C.name,&C.age);
+    scanf("%s%d",D.name,&D.age);
+    printf("%s %d",A.name,A.age);
+    printf("%s %d",B.name,B.age);
+    printf("%s %d",C.name,C.age);
+    printf("%s %d",D.name,D.age);
+
     return 0;
 }
