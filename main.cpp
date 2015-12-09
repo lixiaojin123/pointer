49ea14b2c37a8b1091020e0190ebae4a0c2ce1cf
lixiaojin123 <20141105062l@FanydeiMac13.local>
2015-12-09 18:33:50 +0800

    第十三次提交
    定义一个学生类，学生类中存放的是学生的姓名，性别，年龄，再定义一个尾部指针next，尾部指针next中存放的是下个数组的地址（链表）分别定义一个
    类的四个对象A,B,C,D分别输出类的四个对象的地址（使用链表）

--- main.cpp
+++ main.cpp
@@ -6,22 +6,16 @@
 //  Copyright © 2015年 李晓晋. All rights reserved.
 //
 #include <iostream>
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    int a[3]={1,2,3};
-    printf("a[0]=%d a[1]=%d a[2]=%d\n", a[0],a[1],a[2]);
-    int b[4];
-    printf("%x  %x\n",a,b);
-    int i;
-    for(i=1;i<10;i++)
-    {
-        b[i]=i;
-    }
-    printf("a[0]=%d a[1]=%d a[2]=%d\n",a[0],a[1],a[2]);
-    for(i=1;i<10;i++)
-    {
-         printf("%d\n",b[i]);
-    }
+struct student
+{
+    char name[100];
+    char sex;
+    int age;
+    student *next;
+};
+int main(int argc,const char *argv[]){
+    //insert code here...
+    student A,B,C,D;
+    printf("%x ,%x, %x, %x",&A,&B,&C,&D);
     return 0;
 }
-
